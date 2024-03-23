ae9195c89a0ba8f9f69c3c79b5524647145b1e08
ntruppel <ntruppel@gmail.com>
2024-03-23 22:19:42 +0000

    Updating bsbWeeklyRecap

--- bsbWeeklyRecap.py
+++ bsbWeeklyRecap.py
@@ -13,6 +13,7 @@ from datetime import datetime, timedelta
 from PIL import Image, ImageFont, ImageDraw
 from lib.bsbCommon import getBoxScore
 from lib.common import parseRowStringTextTrue
+import boto3
 
 def get_hitting_stats(score):
     ###
@@ -62,6 +63,120 @@ def get_hitting_stats(score):
     df = df.apply(pd.to_numeric)
     df = df[df.AB != 0]
     return df
+
+def generateScorecardSquare(card,card_draw,color,play_list,w,h,sw,sh):
+
+    ## List plays that reach bases
+    listHome = ['HR', 'x4WP', 'x4', 'z4', 'z4WP', 'SB4']
+    listThird = listHome + ['3B', 'x3', 'x3WP', 'z3', 'z3WP', 'SB3']
+    listSecond = listThird + ['2B', 'x2', 'x2WP', 'z2', 'z2WP', 'SB2']
+    listFirst = listSecond + ['1B', 'BB', 'HBP', 'E2', 'E', 'FC', 'IBB']
+
+    ###
+    # GET COLOR SPECIFICS
+    ###
+    if color == 'blue':
+        single = Image.open('/tmp/bsbScorecardSingleBlue.jpg')
+        textColor = 'black'
+        errColor = '#A20000'
+    elif color == 'gray':
+        single = Image.open('/tmp/bsbScorecardSingleGrey.jpg')
+        textColor = '#2C4997'
+        errColor = '#A20000'
+    elif color == 'red':
+        single = Image.open('/tmp/bsbScorecardSingleRed.jpg')
+        textColor = '#2C4997'
+        errColor = '#A20000'
+    elif color == 'sky':
+        single = Image.open('/tmp/bsbScorecardSingleSky.jpg')
+        textColor = 'black'
+        errColor = '#A20000'
+    elif color == 'white':
+        single = Image.open('/tmp/bsbScorecardSingleWhite.jpg')
+        textColor = '#2C4997'
+        errColor = '#A20000'
+    elif color == 'pinstripe':
+        single = Image.open('/tmp/bsbScorecardSinglePinstripe.jpg')
+        textColor = '#2C4997'
+        errColor = '#A20000'
+
+    card.paste(single, (w,h))
+
+    ###
+    # ITERATE THROUGH PLAYLIST
+    ###
+
+    for row in play_list:
+        
+        ## If out at base, print an 'x'
+        if row['Play'].startswith('x'): 
+            if row['Play'].startswith('x1'):
+                x,y = w+97,h+56
+            if row['Play'].startswith('x2'):
+                x,y = w+67,h+26
+            if row['Play'].startswith('x3'):
+                x,y = w+36,h+56
+            if row['Play'].startswith('x4'):
+                x,y = w+66,h+86
+            card_draw.text((x,y), 'x', font=name_font,fill=errColor)
+            
+            ## We can ignore other baserunning for now
+        elif row['Play'].startswith(('z','SB')): 0
+        
+        ## Everything remaining is a batting play
+        else:
+            
+            ## Calculate the width and height of the text to be drawn, given font size
+            w1 = card_draw.textlength(row['Play'], font=play_font)
+            h1 = 35
+            x = (sw - 15 - w1)/2 + w + 15
+            y = (sh - 35 - h1)/2 + h + 35
+            
+            ## Record play text
+            if row['Play'] in listFirst:
+                card_draw.text((x, y), row['Play'], font=play_font,fill=textColor, align='center')
+            else:
+                card_draw.text((x, y), row['Play'], font=play_font,fill=errColor, align='center')
+                
+            ## Set up Balls and Strikes (if they exist)
+            try:
+                count = str(row['Count'])[:3]
+                if count[0].isdigit():
+                    balls = count[0]
+                    strikes = count[-1]
+                ## Loop through balls
+                b_w = w+13
+                b_h = h+5
+                i = 0
+                while int(balls) > 0:
+                    card_draw.text((b_w + i, b_h), '/', font=bs_font,fill=textColor)
+                    balls = int(balls) - 1
+                    i = i + 22
+                    
+                ## Loop through strikes
+                s_w = w+13
+                s_h = h+25
+                i = 0
+                while int(strikes) > 0:
+                    card_draw.text((s_w + i, s_h), '/', font=bs_font,fill=textColor)
+                    strikes = int(strikes) - 1
+                    i = i + 22
+            except: print("No Balls and Strikes")
+        
+        ## Record basepath movement to first
+        x1,y1 = w+101,h+82
+        x2,y2 = w+70,h+54
+        x3,y3 = w+40,h+82
+        x4,y4 = w+70,h+112
+        if row['Play'] in listFirst:
+            card_draw.line(([(x4,y4),(x1,y1)]), fill=textColor, width = 5)
+        if row['Play'] in listSecond:
+            card_draw.line(([(x1,y1),(x2,y2)]), fill=textColor, width = 5)
+        if row['Play'] in listThird:
+            card_draw.line(([(x2,y2),(x3,y3)]), fill=textColor, width = 5)
+        if row['Play'] in listHome:
+            card_draw.line(([(x3,y3),(x4,y4)]), fill=textColor, width = 5)
+
 
 
 sw = 131                    # Width of the boxscore box
@@ -119,7 +234,7 @@ last_date = (last_date.split(' on '))[1].split(' at')[0]
 today = datetime.now()
 last_datetime = datetime.strptime(last_date, "%B %d, %Y")
 time_dif = today - last_datetime
-if time_dif.days > 7:
+if time_dif.days > 5:
     quit()
 
 ## Reverse the order of the list
@@ -149,7 +264,7 @@ pbp_dfs = []
 last_scores.reverse()
 for score in last_scores:
     hit_stat_dfs.append(get_hitting_stats(score))
-    awayName,homeName,away_df,home_df = getBoxScore(score,platform,'batter')
+    awayName,homeName,away_df,home_df = getBoxScore(score,'batter')
     if awayName in ['LA TECH', 'Louisiana Tech', 'LA Tech']:
         pbp_dfs.append(away_df)
         opponent = homeName
@@ -179,7 +294,6 @@ weekend_df['OPS'] = weekend_df['OPS'].map('{:,.3f}'.format)
 weekend_df = weekend_df.astype({"AB": int, "H": int, "BB": int, "2B": int, "3B": int, "HR": int, "RBI": int})
 weekend_df = weekend_df[['OPS','PA','AB','H','BB','2B','3B','HR','RBI','AVG','OBP','SLG',]]
 weekend_df = weekend_df.sort_values(by='OPS', ascending=False)
-print(weekend_df)
 
 ## Combine the pbp dataframes into one for the series
 for i in range(0,len(pbp_dfs)):
@@ -189,14 +303,15 @@ pbp_df = pd.concat(pbp_dfs)
 pbp_df = pbp_df.reset_index(drop=True)
 pbp_df['Name'] = pbp_df['Name'].str.split('-').str[0]
 pbp_df['Name'] = pbp_df['Name'].str.split(',').str[0]
-print(pbp_df)
+print(pbp_df.loc[pbp_df['Name'] == 'Furr'])
 
 
 ## Grab the individual pbp dfs for each player, in order of OPS (from weekend_df)
 names = []
 indv_dfs = []
 for index,row in weekend_df.iterrows():
-    name = str(index).upper()
+    name = str(index)#.upper()
+    print(name)
     names.append(name)
     indv_dfs.append(pbp_df.loc[pbp_df['Name'] == name])
 
@@ -223,7 +338,7 @@ card_draw = ImageDraw.Draw(card)
 r = 0
 start_w = l_gap
 start_h = t_gap
-colors = ['pinstripe','white','blue','red','sky']
+colors = ['pinstripe','white','blue']
 
 ## Print title info
 card_draw.text((l_gap,5),'Baseball Series Recap', font=large_title_font)
@@ -232,7 +347,7 @@ card_draw.text((l_gap,80),'Scorecard Abomination', font=large_name_font)
 ## Print Team Logo
 box = [(950, 28), (950+111, 28+111)] 
 card_draw.rectangle(box, fill ='black', outline ="white", width=5)
-logo = Image.open(r'in_files/team_logos/' + opponent + '.png')
+logo = Image.open('/tmp/' + opponent + '.png')
 logo = logo.resize((111,111))
 card.paste(logo, (950,28),logo)
 card_draw.text((948,144),'GAMES AGAINST', font=title_font)
@@ -293,123 +408,7 @@ for r in range(0,players):
 
             
     ## We need to run it one more time to get the last square    
-    generateScorecardSquare(card,card_draw,color,play_list,w,h,sw,sh)
+    #generateScorecardSquare(card,card_draw,color,play_list,w,h,sw,sh)
 
-def generateScorecardSquare(card,card_draw,color,play_list,w,h,sw,sh):
-###
-# SQUARE SETUP
-###
-
-## List plays that reach bases
-listHome = ['HR', 'x4WP', 'x4', 'z4', 'z4WP', 'SB4']
-listThird = listHome + ['3B', 'x3', 'x3WP', 'z3', 'z3WP', 'SB3']
-listSecond = listThird + ['2B', 'x2', 'x2WP', 'z2', 'z2WP', 'SB2']
-listFirst = listSecond + ['1B', 'BB', 'HBP', 'E2', 'E', 'FC', 'IBB']
-
-###
-# GET COLOR SPECIFICS
-###
-if color == 'blue':
-    single = Image.open('/tmp/bsbScorecardSingle/bsbScorecardSingleBlue.jpg')
-    textColor = 'black'
-    errColor = '#A20000'
-elif color == 'gray':
-    single = Image.open('/tmp/bsbScorecardSingle/bsbScorecardSingleGrey.jpg')
-    textColor = '#2C4997'
-    errColor = '#A20000'
-elif color == 'red':
-    single = Image.open('/tmp/bsbScorecardSingle/bsbScorecardSingleRed.jpg')
-    textColor = '#2C4997'
-    errColor = '#A20000'
-elif color == 'sky':
-    single = Image.open('/tmp/bsbScorecardSingle/bsbScorecardSingleSky.jpg')
-    textColor = 'black'
-    errColor = '#A20000'
-elif color == 'white':
-    single = Image.open('/tmp/bsbScorecardSingle/bsbScorecardSingleWhite.jpg')
-    textColor = '#2C4997'
-    errColor = '#A20000'
-elif color == 'pinstripe':
-    single = Image.open('/tmp/bsbScorecardSingle/bsbScorecardSinglePinstripe.jpg')
-    textColor = '#2C4997'
-    errColor = '#A20000'
-
-card.paste(single, (w,h))
-
-
-###
-# ITERATE THROUGH PLAYLIST
-###
-
-for row in play_list:
-    
-    ## If out at base, print an 'x'
-    if row['Play'].startswith('x'): 
-        if row['Play'].startswith('x1'):
-            x,y = w+97,h+56
-        if row['Play'].startswith('x2'):
-            x,y = w+67,h+26
-        if row['Play'].startswith('x3'):
-            x,y = w+36,h+56
-        if row['Play'].startswith('x4'):
-            x,y = w+66,h+86
-        card_draw.text((x,y), 'x', font=name_font,fill=errColor)
-        
-        ## We can ignore other baserunning for now
-    elif row['Play'].startswith(('z','SB')): 0
-    
-    ## Everything remaining is a batting play
-    else:
-        
-        ## Calculate the width and height of the text to be drawn, given font size
-        w1, h1 = card_draw.textsize(row['Play'], font=play_font)
-        x = (sw - 15 - w1)/2 + w + 15
-        y = (sh - 35 - h1)/2 + h + 35
-        
-        ## Record play text
-        if row['Play'] in listFirst:
-            card_draw.text((x, y), row['Play'], font=play_font,fill=textColor, align='center')
-        else:
-            card_draw.text((x, y), row['Play'], font=play_font,fill=errColor, align='center')
-            
-        ## Set up Balls and Strikes (if they exist)
-        try:
-            count = str(row['Count'])[:3]
-            if count[0].isdigit():
-                balls = count[0]
-                strikes = count[-1]
-            ## Loop through balls
-            b_w = w+13
-            b_h = h+5
-            i = 0
-            while int(balls) > 0:
-                card_draw.text((b_w + i, b_h), '/', font=bs_font,fill=textColor)
-                balls = int(balls) - 1
-                i = i + 22
-                
-            ## Loop through strikes
-            s_w = w+13
-            s_h = h+25
-            i = 0
-            while int(strikes) > 0:
-                card_draw.text((s_w + i, s_h), '/', font=bs_font,fill=textColor)
-                strikes = int(strikes) - 1
-                i = i + 22
-        except: print("No Balls and Strikes")
-    
-    ## Record basepath movement to first
-    x1,y1 = w+101,h+82
-    x2,y2 = w+70,h+54
-    x3,y3 = w+40,h+82
-    x4,y4 = w+70,h+112
-    if row['Play'] in listFirst:
-        card_draw.line(([(x4,y4),(x1,y1)]), fill=textColor, width = 5)
-    if row['Play'] in listSecond:
-        card_draw.line(([(x1,y1),(x2,y2)]), fill=textColor, width = 5)
-    if row['Play'] in listThird:
-        card_draw.line(([(x2,y2),(x3,y3)]), fill=textColor, width = 5)
-    if row['Play'] in listHome:
-        card_draw.line(([(x3,y3),(x4,y4)]), fill=textColor, width = 5)
-        
 card.save('/tmp/bsbWeeklyRecap.png')
 s3.upload_file('/tmp/bsbWeeklyRecap.png', 'gtpdd', 'bsbWeeklyRecap.png')
